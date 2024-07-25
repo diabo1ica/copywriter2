d17fee21baf65e323e01d5f083f67ff32a904eaf
d1abolica <garyang2002@gmail.com>
2024-07-26 01:18:23 +1000

    Configured production environment files againnn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   
+  
   config.require_master_key = true
   config.secret_key_base = Rails.application.credentials.dig(:production, :secret_key_base)
 
